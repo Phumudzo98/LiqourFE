e0b2116793489aab4c3af12d324a257ef74fe60f
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-06-10 10:03:33 +0200

    New Changes

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
+  navigateToTasks(){
+    this.route.navigate(['my-tasks'])
+  }
+
+  navigateToUpdate(){
+    this.route.navigate(['update-gis'])
+  }
+
+  navigateToInspection(){
+    this.route.navigate(['inspections'])
+  }
+
+  navigateToComplaints(){
+    this.route.navigate(['complaints'])
+  }
+  navigateToNavigate(){
+    this.route.navigate(['navigate-to-outlet'])
+  }
 }
