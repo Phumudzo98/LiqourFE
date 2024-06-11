71e8dda7618de17f65dd304a5666b7020777ac6c
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-06-11 09:24:27 +0200

    New Changes

--- src/app/Outlet/outlet-dashboard/outlet-dashboard.page.ts
+++ src/app/Outlet/outlet-dashboard/outlet-dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-outlet-dashboard',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutletDashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
-  ngOnInit() {
+  private currentIndex: number = 0;
+  private slides: HTMLElement[] = []; 
+  private dots: HTMLElement[] = [];
+
+   ngOnInit() {
+    this.slides = Array.from(document.querySelectorAll('.slide')) as HTMLElement[];
+    this.dots = Array.from(document.querySelectorAll('.dot')) as HTMLElement[];
+    this.startSlideShow();
+  }
+
+
+  private startSlideShow() {
+    if (this.slides.length === 0) return;
+
+    setInterval(() => {
+      this.slides[this.currentIndex].classList.remove('visible');
+      this.dots[this.currentIndex].classList.remove('active');
+      this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+      this.slides[this.currentIndex].classList.add('visible');
+      this.dots[this.currentIndex].classList.add('active');
+    }, 3000);
+  }
+
+
+  navigateToTasks(){
+    this.route.navigate(['my-tasks'])
+  }
+
+  navigateToUpdate(){
+    this.route.navigate(['update-gis'])
+  }
+
+  navigateToInspection(){
+    this.route.navigate(['inspections'])
+  }
+
+  navigateToComplaints(){
+    this.route.navigate(['complaints'])
+  }
+  navigateToNavigate(){
+    this.route.navigate(['navigate-to-outlet'])
   }
 
 }
