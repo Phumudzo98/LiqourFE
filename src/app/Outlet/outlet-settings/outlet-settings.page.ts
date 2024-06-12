984fa0601ab4d9972faabfcfeb4825ceabf25c4d
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-06-12 13:52:00 +0200

    New Changes

--- src/app/Outlet/outlet-settings/outlet-settings.page.ts
+++ src/app/Outlet/outlet-settings/outlet-settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-outlet-settings',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutletSettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  currentForm: string = 'landing';
+
+  toggleForms(form: string) {
+    this.currentForm = form;
+  }
+
+ 
 
 }
