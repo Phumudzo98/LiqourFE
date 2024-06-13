62938cf526bbe084d2ce077da26d4c0782f02e98
MFACTORY\nhlan <nhlanhlakhoza05@gmail.com>
2024-06-13 10:11:18 +0200

    update

--- src/app/pages/signin/signin.page.ts
+++ src/app/pages/signin/signin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+  email:string="";
+  password!: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  sign(){
+    if(this.email === "mkay1@gmail.com")
+      {
+        this.router.navigate(['dashboard'])
+      }
+      else{
+        this.router.navigate(['outlet-dashboard'])
+
+      }
+  }
 }
