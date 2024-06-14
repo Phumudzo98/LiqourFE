91e843b4fcd80eb2728405e58d5e49d3e5c165d0
MFACTORY\nhlan <nhlanhlakhoza05@gmail.com>
2024-06-14 08:43:12 +0200

    update

--- src/app/pages/navigate-to-outlet/navigate-to-outlet.page.ts
+++ src/app/pages/navigate-to-outlet/navigate-to-outlet.page.ts
@@ -11,5 +11,40 @@ export class NavigateToOutletPage implements OnInit {
 
   ngOnInit() {
   }
+  outlets = [
+    {
+      imgSrc: '../../../assets/Images/kwa coca.jpeg',
+      header: 'Kwa Coca Tavern',
+      details: 'ECP08498/03017/OO <br>On & Off Consumption',
+      iconSrc: '../../../assets/Images/Group 88.svg',
+    },
+    {
+      imgSrc: '../../../assets/Images/pllas.jpeg',
+      header: 'POLLAS TAVERN',
+      details: 'ECP08500/03020/OO <br>On & Off Consumption',
+      iconSrc: '../../../assets/Images/Group 88.svg',
+    },
+ 
 
+    {
+      imgSrc: '../../../assets/Images/viva.jpeg',
+      header: 'VIVAS TAVERN',
+      details: 'ECP08507/90454/OO <br>On & Off Consumption',
+      iconSrc: '../../../assets/Images/Group 88.svg',
+    },
+    {
+      imgSrc: '../../../assets/Images/shakis.jpeg',
+      header: 'SAKHIS TAVERN',
+      details: 'ECP08517/03033/OO <br>On & Off Consumption',
+      iconSrc: '../../../assets/Images/Group 88.svg',
+    },
+   
+    {
+      imgSrc: '../../../assets/Images/burguer nn.jpeg',
+      header: 'THE BURGER INN',
+      details: 'ECP00852/90454/ON <br>On Consumption',
+      iconSrc: '../../../assets/Images/Group 88.svg',
+    },
+    
+  ];
 }
