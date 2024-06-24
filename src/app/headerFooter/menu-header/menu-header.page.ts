10ea4a5f5a1ff02cd324c8237d185439de4df10c
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-06-24 13:38:33 +0200

    New Changes

--- src/app/headerFooter/menu-header/menu-header.page.ts
+++ src/app/headerFooter/menu-header/menu-header.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-header',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-header.page.scss'],
 })
 export class MenuHeaderPage implements OnInit {
+  isDashboard: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.isDashboard = this.router.url === '/dashboard';
+  }
+
+  toDashboard()
+  {
+    this.router.navigate(['dashboard']);
   }
 
 }
+
