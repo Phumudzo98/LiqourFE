55f7168373919c33c3ac4b89b6e82f9fa7b7bad6
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-06-26 12:38:16 +0200

    New Changes

--- src/app/pages/complete-inspection/complete-inspection.page.ts
+++ src/app/pages/complete-inspection/complete-inspection.page.ts
@@ -17,10 +17,11 @@ import { ViewImagePage } from '../view-image/view-image.page';
 export class CompleteInspectionPage implements OnInit {
 
   selectedOption: string = '';
-  uploadedFiles: { name: string, size: number }[] = [];
+  reportFiles: { name: string, size: number }[] = [];
+  noticeFiles: { name: string, size: number }[] = [];
   currentForm: string = 'landing';
-  selectedRadioValue: string | null = null; // Initialize to null or the default value you want
-  inputVisible: boolean = true; // Add this property
+  selectedRadioValue: string | null = null; 
+  inputVisible: boolean = true; 
 
   @ViewChild('fileInput', { static: false })
   fileInput!: ElementRef<HTMLInputElement>;
@@ -29,6 +30,7 @@ export class CompleteInspectionPage implements OnInit {
   caseId: any;
   caseNo: any;
   imageSources: string[] = [];
+
   inspectionReport:any;
   reportDoc:any;
   noticeDoc:any;
@@ -44,41 +46,42 @@ export class CompleteInspectionPage implements OnInit {
     private route: ActivatedRoute,
     private actionSheetController: ActionSheetController,
     private modalController: ModalController,
+
   ) {
     this.completeReportForm = this.fb.group({
-      personContacted: ['', Validators.required],
-      inspectionDate: ['', [Validators.required]],
-      latitude: ['', [Validators.required]],
-      interestInLiquorTrade: ['', [Validators.required]],
-      issuedComplience: ['', [Validators.required]],
-      complaintsReceived: ['', [Validators.required]],
-      longitude: ['', [Validators.required]],
+      contactPerson: ['', Validators.required],
+      inspectionDate: ['', Validators.required],
       appointmentSet: ['', Validators.required],
-      personConsulted: ['', Validators.required],
-      indicatedParticularPerson: ['', [Validators.required]],
-      personFoundConfirmed: ['', [Validators.required]],
-      rightToOccupy: ['', [Validators.required]],
-      formServedToWardCommittee: ['', Validators.required],
-      formServedToWardCouncillor: ['', [Validators.required]],
-      wardCommitteReport: ['', [Validators.required]],
-      communityConsulted: ['', [Validators.required]],
-      educationalInstitutionWithin100m: ['', [Validators.required]],
-      premisesInIndicatedAddress: ['', [Validators.required]],
-      formServedAtEducationalInstitution: ['', [Validators.required]],
-      placeOfWorshipWithin100m: ['', [Validators.required]],
-      formServedAtPlaceOfWorship: ['', [Validators.required]],
-      recommendationForRegistration: ['', Validators.required],
-      comments: ['', Validators.required],
-      futurePreInspectionDate: ['', Validators.required],
-      lease: ['', Validators.required],
-      premiseInLineWithPlan: ['', Validators.required],
-      premisesSuitedForCategory: ['', Validators.required],
-      abulutionFacilityWorking: ['', Validators.required],
-      readyToCommenceWithBusiness: ['', Validators.required],
+      consultedOrFound: ['', Validators.required],
+      applicantIndicatedPersonAtPremises: ['', Validators.required],
+      canPersonBeFound: ['', Validators.required],
+      interestInLiquorTrade: ['', Validators.required],
+      issuedCompliance: ['', Validators.required],
+      complaintsReceived: ['', Validators.required],
+      rightToOccupy: ['', Validators.required],
+      leaseAttached: ['', Validators.required],
+      situatedInRightAddress: ['', Validators.required],
+      inLineWithSubmittedApplication: ['', Validators.required],
+      premisesSuitable: ['', Validators.required],
+      latitude: ['', Validators.required],
+      longitude: ['', Validators.required],
+      ablutionFacilityInOrder: ['', Validators.required],
+      readyForBusiness: ['', Validators.required],
+      formServedToCorrectWardCommittee: ['', Validators.required],
+      confirmedByCouncillor: ['', Validators.required],
+      wardCommitteeReport: ['', Validators.required],
+      communityConsultation: ['', Validators.required],
+      educationalInstitution: ['', Validators.required],
+      formServedAtEducationInstitution: ['', Validators.required],
+      placeOfWorship: ['', Validators.required],
+      formServedAtPlaceOfWorship: ['', Validators.required],
       recommendation: ['', Validators.required],
-      reportDoc:['', Validators.required],
-      noticeDoc:['', Validators.required]
+      futureInspectionDate: ['', Validators.required],
+      comments: ['', Validators.required]
     });
+    
+      
+    
   }
 
   ngOnInit() {
@@ -103,18 +106,18 @@ export class CompleteInspectionPage implements OnInit {
     if (this.noticeDoc)
       formData.append('notice', this.noticeDoc.file);
     
-    
-
-    let token = localStorage.getItem("userToken")
-    const newHeader = {
-      "Authorization": "Bearer " + token,
-      "Accept": "*/*"
+    let token = localStorage.getItem("userToken") 
+    const newHeader={
+      "Authorization":"Bearer "+token, 
+      "Accept":"/"
     }
 
     let url = "https://system.eclb.co.za/eclb2/api/general/complete-inspection-report/" + this.caseNo
 
     this.http.post(url,formData, {headers: newHeader}).subscribe(response=>{
       console.log(response);
+
+      this.router.navigate(['/thank-you'])
     }, error => {
       console.log(error);
     })
@@ -150,14 +153,14 @@ export class CompleteInspectionPage implements OnInit {
       if (this.isFileUploaded(file.name)) {
         await this.presentFileExistsAlert();
       } else {
-        this.uploadedFiles.push({ name: file.name, size: file.size });
-        this.inputVisible = false; // Hide the input
+        this.reportFiles.push({ name: file.name, size: file.size });
+        this.inputVisible = false; 
       }
     }
   }
 
   isFileUploaded(fileName: string): boolean {
-    return this.uploadedFiles.some(file => file.name === fileName);
+    return this.reportFiles.some(file => file.name === fileName);
   }
 
   async presentFileExistsAlert() {
@@ -200,8 +203,63 @@ export class CompleteInspectionPage implements OnInit {
   }
 
   deleteItem(index: number) {
-    this.uploadedFiles.splice(index, 1);
-    if (this.uploadedFiles.length === 0) {
+    this.reportFiles.splice(index, 1);
+    if (this.reportFiles.length === 0) {
+      this.inputVisible = true; 
+    }
+  }
+
+  // File
+
+  
+  async onFileSelectedRecommendation(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      if (this.noticeFiles.length > 0) {
+        this.noticeFiles.splice(0, 1, { name: file.name, size: file.size });
+      } else {
+        this.noticeFiles.push({ name: file.name, size: file.size });
+      }
+      this.inputVisible = false; 
+    }
+  }
+
+  isFileUploadedNotice(fileName: string): boolean {
+    return this.noticeFiles.some(file => file.name === fileName);
+  }
+
+ 
+
+
+
+  async presentAlertConfirmNotice(index: number) {
+    const alert = await this.alertController.create({
+      header: 'Confirm Deletion',
+      message: 'Are you sure you want to delete this document?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            this.deleteItemNotice(index);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+      ]
+    });
+
+    await alert.present();
+  }
+
+  deleteItemNotice(index: number) {
+    this.noticeFiles.splice(index, 1);
+    if (this.noticeFiles.length === 0) {
       this.inputVisible = true; 
     }
   }
