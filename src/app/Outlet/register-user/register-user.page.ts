c284553d58ca457dc54d225644c33d78cfb5e8d3
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-07-12 09:17:43 +0200

    New Changes

--- src/app/Outlet/register-user/register-user.page.ts
+++ src/app/Outlet/register-user/register-user.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-register-user',
@@ -10,7 +12,8 @@ import { Router } from '@angular/router';
 export class RegisterUserPage implements OnInit {
 
   registerForm: FormGroup;
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private spinner: NgxSpinnerService
+  ) {
     this.registerForm = this.fb.group({
       fullName: ['', Validators.required],
       email: ['', Validators.required],
@@ -26,10 +29,15 @@ export class RegisterUserPage implements OnInit {
   ngOnInit() {
   }
 
-   toLogin()
-   {
-    this.router.navigate(["signin"])
-   }
+  toLogin() {
+    this.spinner.show(); 
+  
+    setTimeout(() => {
+      this.spinner.hide(); 
+      this.router.navigate(['signin']);
+    }, 2000);
+  }
+  
   get register (){return this.registerForm.controls;}
 
 
