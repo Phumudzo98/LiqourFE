daa2ae213d708ac95d1020029926d206e5043329
Mkay\Mukovhe <mukovhemufamadi@gmail.com>
2024-07-24 12:02:55 +0200

    New

--- src/app/pages/update-gis/update-gis.page.ts
+++ src/app/pages/update-gis/update-gis.page.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner'; // Import NgxSpinnerService
 import { headersSecure } from 'src/app/util/service/const';
 
-
 @Component({
   selector: 'app-update-gis',
   templateUrl: './update-gis.page.html',
@@ -16,34 +15,26 @@ export class UpdateGisPage implements OnInit {
   
   filteredOutlets: any[] = [];
 
-  outlets:any[]=[];
+  outlets: any[] = [];
 
-  
+  url: string = "https://system.eclb.co.za/eclb2//api/general/save-gis"
+  url2: string = "https://system.eclb.co.za/eclb2/api/general/get-inbox";
 
-  url:string="https://system.eclb.co.za/eclb2//api/general/save-gis"
   constructor(private route: Router, private spinner: NgxSpinnerService, private http: HttpClient) { }
-   // Inject NgxSpinnerService
-
-  url2="https://system.eclb.co.za/eclb2/api/general/get-inbox";
-
 
   ngOnInit() {
     this.fetchData(); // Call fetchData on component initialization
 
-
-    this.http.get(this.url2,{headers: headersSecure}).subscribe( (response: any)=>
-      {
+    this.http.get(this.url2, { headers: headersSecure }).subscribe(
+      (response: any) => {
         console.log(response);
-
-        this.outlets=response;
+        this.outlets = response;
         this.filteredOutlets = this.outlets;
-        
-      },error=>
-        {
-          console.log(error);
-          
-        }
-    )
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
   fetchData() {
@@ -51,19 +42,18 @@ export class UpdateGisPage implements OnInit {
 
     // Simulating asynchronous data fetching (replace with actual HTTP request)
     setTimeout(() => {
-      
       this.spinner.hide();
       this.filteredOutlets = this.outlets; // Assign data to filteredOutlets
-       // Hide spinner after data fetch
     }, 2000); // Simulate 2 seconds delay (replace with actual data fetch logic)
   }
-
+  
   filterOutlets() {
     const term = this.searchTerm.toLowerCase();
     this.filteredOutlets = this.outlets.filter((outlet) => 
-      outlet.header.toLowerCase().startsWith(term)
+      outlet.outletName.toLowerCase().startsWith(term)
     );
   }
+  
 
   navigateToBack() {
     this.route.navigate(['dashboard']);
